4f2c5bbd8d2ae45fa37e55801b8c9e6e58b11c36
Keartroth <mecarrolljr@gmail.com>
2020-04-21 11:09:41 -0500

    added an add employee form that allows a user to add employees; refactored dependent modules.

--- src/components/employee/EmployeeList.js
+++ src/components/employee/EmployeeList.js
@@ -4,21 +4,27 @@ import { LocationContext } from "../location/LocationProvider"
 import Employee from "./Employee"
 import "./Employee.css"
 
-export default () => {
+export default (props) => {
     const { employees } = useContext(EmployeeContext)
     const { locations } = useContext(LocationContext)
 
     return (
-        <div className="employees">
-        {
-            employees.map(employee => {
-                const clinic = locations.find(l => l.id === employee.locationId)
+        <article className="employees">
+            <h1>Employees</h1>
+            <button onClick={() => props.history.push("/employees/create")}>
+                Add Employee
+        </button>
+            <section className="employeeList">
+                {
+                    employees.map(employee => {
+                        const clinic = locations.find(l => l.id === employee.locationId)
 
-                return <Employee key={employee.id}
-                                location={clinic}
-                                employee={employee} />
-            })
-        }
-        </div>
+                        return <Employee key={employee.id}
+                            location={clinic}
+                            employee={employee} />
+                    })
+                }
+            </section>
+        </article>
     )
 }
