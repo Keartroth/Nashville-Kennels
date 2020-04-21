f481a34474ea59829ba117a6176d2d6bd58397c4
Keartroth <mecarrolljr@gmail.com>
2020-04-20 22:13:13 -0500

    added a functioning nav bar.

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,58 +1,12 @@
 import React from "react"
-import "./animal/Animal.css"
-import "./employee/Employee.css"
-import "./location/Location.css"
-import "./customer/Customer.css"
-import LocationList from "./location/LocationList"
-import { LocationProvider } from "./location/LocationProvider"
-import AnimalList from "./animal/AnimalList"
-import { AnimalProvider } from "./animal/AnimalProvider"
-import { CustomerProvider } from "./customer/CustomerProvider"
-import CustomerList from "./customer/CustomerList"
-import { EmployeeProvider } from "./employee/EmployeeProvider"
-import EmployeeList from "./employee/EmployeeList"
+import { Route } from "react-router-dom"
+import NavBar from "./nav/NavBar"
+import ApplicationViews from "./ApplicationViews"
+import "./Kennel.css"
 
 export default () => (
     <>
-        <h2>Nashville Kennels</h2>
-        <small>Loving care when you're not there.</small>
-        <address>
-            <div>Visit Us at the Nashville North Location</div>
-            <div>500 Puppy Way</div>
-        </address>
-
-        <h2>Animals</h2>
-        <article className="animals">
-            <AnimalProvider>
-                <LocationProvider>
-                    <CustomerProvider>
-                        <AnimalList />
-                    </CustomerProvider>
-                </LocationProvider>
-            </AnimalProvider>
-        </article>
-
-        <h2>Employee</h2>
-        <article className="employees">
-            <EmployeeProvider>
-                <LocationProvider>
-                    <EmployeeList />
-                </LocationProvider>
-            </ EmployeeProvider>
-        </article>
-
-        <h2>Locations</h2>
-        <article className="locations">
-            <LocationProvider>
-                <LocationList />
-            </LocationProvider>
-        </article>
-
-        <h2>Customers</h2>
-        <article className="customers">
-            <CustomerProvider>
-                <CustomerList />
-            </CustomerProvider>
-        </article>
+        <NavBar />
+        <ApplicationViews />
     </>
 )
